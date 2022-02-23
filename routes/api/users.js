f39bd54d9073eb32227c16f05b36aa276b79feda
anas bhih <bhih.anas@gmail.com>
2022-02-23 20:51:38 +0200

    finish auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
 
 // Load User model
 const User = require('../../models/User');
@@ -37,6 +39,7 @@ router.post('/', [
                 d: 'mm'
             })
 
+            // create user
             user = new User({
                 name,
                 email,
@@ -44,13 +47,26 @@ router.post('/', [
                 password
             });
 
-            //Encrypt password
+            // encrypt password
             const salt = await bcrypt.genSalt(10);
             user.password = await bcrypt.hash(password, salt);
 
             await user.save();
 
-            res.send('User registered');
+            // jwt
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload, config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            )
 
         } catch (err) {
             console.error(err.message);
