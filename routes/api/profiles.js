37262d7d7c6e7f5305fb95ed46e8d0b58fd05ce5
anas bhih <bhih.anas@gmail.com>
2022-02-27 20:56:48 +0200

    profile

--- routes/api/profiles.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// @route   api/profiles/test
-// @desc    Test profiles route
-// @access  Public
-router.get('/', (req,res) => res.json({msg: "profiles works!"}));
-
-module.exports = router;
