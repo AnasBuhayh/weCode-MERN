a8afff5d3a9ac698257e0f5e2314118a80a5e07f
anas bhih <bhih.anas@gmail.com>
2022-03-21 21:56:59 +0200

    alerts

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './component/layout/Navbar';
 import Landing from './component/layout/Landing';
 import Login from './component/auth/Login';
 import Register from './component/auth/Register';
+import Alert from './component/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -13,14 +14,13 @@ import './App.css';
 const App = () => (
   <Provider store={store}>
     <Router>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/login" element={<Login />} />
-        </Routes>
-      </Fragment>
+      <Navbar />
+      <Alert />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
     </Router>
   </Provider>
 
