de55c82babb049171c4f0a082ee801f2d1c9a6f3
anas bhih <bhih.anas@gmail.com>
2022-04-03 20:46:21 +0200

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './component/layout/Navbar';
 import Landing from './component/layout/Landing';
@@ -8,22 +8,34 @@ import Alert from './component/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken'
 
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Navbar />
-      <Alert />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  </Provider>
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
-);
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
+
+  )
+};
 
 export default App;
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,9 +1,35 @@
 import axios from 'axios';
 import { setAlert } from './alert'
+import setAuthToken from '../utils/setAuthToken';
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from './types';
+
+
+// Load User
+export const loadUser = () => async dispatch => {
+    if (localStorage.token) {
+        setAuthToken(localStorage.token);
+    }
+
+    try {
+        const res = await axios.get('/api/auth');
+
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR
+        })
+    }
+}
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
@@ -11,7 +37,7 @@ export const register = ({ name, email, password }) => async dispatch => {
         headers: {
             'Content-Type': 'application/json'
         }
-    }
+    };
 
     const body = JSON.stringify({ name, email, password });
 
@@ -23,6 +49,8 @@ export const register = ({ name, email, password }) => async dispatch => {
             payload: res.data
         });
 
+        dispatch(loadUser());
+
     } catch (err) {
         const errors = err.response.data.errors;
 
@@ -33,6 +61,40 @@ export const register = ({ name, email, password }) => async dispatch => {
 
         dispatch({
             type: REGISTER_FAIL
+        });
+    }
+}
+
+// Login User
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ email, password });
+
+    try {
+        const res = await axios.post('/api/auth', body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+
+        dispatch(loadUser());
+
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            console.log("errors")
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: LOGIN_FAIL
         });
     }
 }
